ab8a0cd17f5eb88586337396a8ce9e9de84136ad
pursue503 <pursue503@naver.com>
2022-10-22 18:40:29 +0900

    Add Toby-Reactive-Programming-01 Complete

--- src/main/java/com/reactive/study/object/PubSub.java
+++ src/main/java/com/reactive/study/object/PubSub.java
@@ -1,8 +1,8 @@
 package com.reactive.study.object;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Flow;
+import java.util.Iterator;
+import java.util.concurrent.*;
 import java.util.concurrent.Flow.Publisher;
 import java.util.concurrent.Flow.Subscriber;
 
@@ -10,18 +10,35 @@ import java.util.concurrent.Flow.Subscriber;
  * publisher subscriber
  */
 public class PubSub {
-    public static void main(String[] args) {
-        // publisher <- Observable
-        // Subscriber <- Observer
+    public static void main(String[] args) throws InterruptedException {
+
 
         Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
         Publisher publisher = new Publisher() {
             @Override
             public void subscribe(Subscriber subscriber) {
+                Iterator<Integer> it = itr.iterator();
                 subscriber.onSubscribe(new Flow.Subscription() {
                     @Override
                     public void request(long n) {
+                        executorService.execute(() -> {
+                            int i = 0;
+                            try {
+                                while (i++ < n) {
+                                    if (it.hasNext()) {
+                                        subscriber.onNext(it.next());
+                                    } else {
+                                        subscriber.onComplete();
+                                        break;
+                                    }
+                                }
+                            } catch (RuntimeException e) {
+                                subscriber.onError(e);
+                            }
+
+                        });
 
                     }
 
@@ -34,28 +51,34 @@ public class PubSub {
         };
 
         Subscriber<Integer> subscriber = new Subscriber<Integer>() {
+            Flow.Subscription subscription;
             @Override
             public void onSubscribe(Flow.Subscription subscription) {
-
+                System.out.println("onSubscribe");
+                this.subscription = subscription;
+                this.subscription.request(1);
             }
 
             @Override
             public void onNext(Integer item) {
-
+                System.out.println(Thread.currentThread().getName() +  " onNext : " + item);
+                this.subscription.request(1);
             }
 
             @Override
             public void onError(Throwable throwable) {
-
+                System.out.println("onError : " + throwable.getMessage());
             }
 
             @Override
             public void onComplete() {
-
+                System.out.println("onComplete");
             }
         };
 
         publisher.subscribe(subscriber);
 
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+        executorService.shutdown();
     }
 }
