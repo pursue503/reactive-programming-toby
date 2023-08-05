d8b40cff8d22dd08e2428b7c6efcd56dd8791fc9
pursue503 <pursue503@naver.com>
2023-08-05 17:39:42 +0900

    Update Spring boot Version && Add Step3, Step4

---
+++ src/main/java/com/reactive/study/step4/FutureEx.java
@@ -0,0 +1,12 @@
+package com.reactive.study.step4;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Future;
+
+@Slf4j
+public class FutureEx {
+    public static void main(String[] args) {
+        Future
+    }
+}
